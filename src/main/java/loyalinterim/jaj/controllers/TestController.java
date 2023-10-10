118585a9d548a5f5cafcd2474b19fee70b52eb47
AmarKoric <amarkoric@hotmail.com>
2023-10-10 15:16:08 +0200

    Added controller and made it so the Spring Boot application can be run

---
+++ src/main/java/loyalinterim/jaj/controllers/TestController.java
@@ -0,0 +1,13 @@
+package loyalinterim.jaj.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping(value="/")
+    public String hello(){
+        return "Hello World!!";
+    }
+}
